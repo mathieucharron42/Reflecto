ac486556feb97398f8b35887528765dbe97324a2
Mathieu Charron <mathieucharron42@gmail.com>
2021-04-03 15:52:38 -0400

    Implement ArrayToTupleProjection

--- ReflectoCommon/Utils/CollectionExt.h
+++ ReflectoCommon/Utils/CollectionExt.h
@@ -12,12 +12,12 @@ namespace Reflecto
 	{
 		namespace CollectionExt
 		{
-			/*template<
+			template<
 				typename element_t,
 				std::size_t array_size,
 				std::size_t... i
 			>
-				constexpr auto ArrayToTuple(const std::array<element_t, array_size>& array, std::index_sequence<i...>)
+			constexpr auto ArrayToTuple(const std::array<element_t, array_size>& array, std::index_sequence<i...>)
 			{
 				return std::make_tuple(array[i]...);
 			}
@@ -29,15 +29,15 @@ namespace Reflecto
 			constexpr auto ArrayToTuple(const std::array<element_t, array_size>& array)
 			{
 				return ArrayToTuple(array, std::make_index_sequence<array_size>{});
-			}*/
+			}
 
 			template<
 				typename element_t,
 				std::size_t array_size,
-				typename projection_t = Reflecto::Utils::Identity,
+				typename projection_t,
 				std::size_t... i
 			>
-			constexpr auto ArrayToTuple(const std::array<element_t, array_size>& array, projection_t projection, std::index_sequence<i...>)
+				constexpr auto ArrayToTupleProjection(const std::array<element_t, array_size>& array, projection_t projection, std::index_sequence<i...>)
 			{
 				return std::make_tuple(projection(array[i])...);
 			}
@@ -47,9 +47,30 @@ namespace Reflecto
 				std::size_t array_size,
 				typename projection_t = Reflecto::Utils::Identity
 			>
-			constexpr auto ArrayToTuple(const std::array<element_t, array_size>& array, projection_t projection = projection_t{})
+				constexpr auto ArrayToTupleProjection(const std::array<element_t, array_size>& array, projection_t projection)
 			{
-				return ArrayToTuple(array, projection, std::make_index_sequence<array_size>{});
+				return ArrayToTupleProjection(array, projection, std::make_index_sequence<array_size>{});
+			}
+
+			template<
+				typename element_t,
+				std::size_t array_size,
+				typename ... projections_t,
+				std::size_t... i
+			>
+			constexpr auto ArrayToTupleProjections(const std::array<element_t, array_size>& array, const std::tuple<projections_t...> projections, std::index_sequence<i...>)
+			{
+				return std::make_tuple(std::get<i>(projections)(array[i])...);
+			}
+
+			template<
+				typename element_t,
+				std::size_t array_size,
+				typename ... projections_t
+			>
+			constexpr auto ArrayToTupleProjections(const std::array<element_t, array_size>& array, const std::tuple<projections_t...> projections)
+			{
+				return ArrayToTupleProjections(array, projections, std::make_index_sequence<array_size>{});
 			}
 		}
 	}
--- ReflectoCommonTest/Utils/CollectionExt.cpp
+++ ReflectoCommonTest/Utils/CollectionExt.cpp
@@ -28,17 +28,32 @@ namespace Reflecto
 					Assert::AreEqual(std::get<2>(array), std::get<2>(tuple), L"Element are unexpectedly different");
 				}
 
-				TEST_METHOD(ArrayToTupleConvertTest)
+				TEST_METHOD(ArrayToTupleSingleProjectionTest)
 				{
 					
 					std::array<int32_t, 3> array = { 42,5,29 };
-					std::tuple<std::string, std::string, std::string> tuple = Reflecto::Utils::CollectionExt::ArrayToTuple(array, [] (int32_t i) {
+					std::tuple<std::string, std::string, std::string> tuple = Reflecto::Utils::CollectionExt::ArrayToTupleProjection(array, [] (int32_t i) {
 						return std::to_string(i+1);
 					});
 
-					Assert::AreEqual(std::string("43"), std::get<0>(tuple), L"Element are unexpectedly different");
-					Assert::AreEqual(std::string("6"), std::get<1>(tuple), L"Element are unexpectedly different");
-					Assert::AreEqual(std::string("30"), std::get<2>(tuple), L"Element are unexpectedly different");
+					Assert::AreEqual(std::string("43"), std::get<0>(tuple), L"Element 0 is unexpected");
+					Assert::AreEqual(std::string("6"), std::get<1>(tuple), L"Element 1 is unexpected");
+					Assert::AreEqual(std::string("30"), std::get<2>(tuple), L"Element 2 is unexpected");
+				}
+
+				TEST_METHOD(ArrayToTupleMultipleProjectionTest)
+				{
+					std::array<int32_t, 3> array = { 42, 5, 29 };
+					auto projections = std::make_tuple(
+						[] (int32_t i) { return std::to_string(i);},
+						[] (int32_t i) { return i;},
+						[] (int32_t i) { return float(i);}
+					);
+					std::tuple<std::string, int32_t, float> tuple = Reflecto::Utils::CollectionExt::ArrayToTupleProjections(array, projections);
+
+					Assert::AreEqual(std::string("42"), std::get<0>(tuple), L"Element 0 is unexpected");
+					Assert::AreEqual(5, std::get<1>(tuple), L"Element 1 is unexpected");
+					Assert::AreEqual(29.f, std::get<2>(tuple), L"Element 2 is unexpected");
 				}
 			};
 		}
